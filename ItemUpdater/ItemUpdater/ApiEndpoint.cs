74d487d59b98ac0eb51c7b1f49c22303b594d65a
shaun <shaun@bluebit.com.au>
2024-02-06 09:44:50 +1100

    Emby item updater plugin

---
+++ ItemUpdater/ItemUpdater/ApiEndpoint.cs
@@ -0,0 +1,111 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common;
+using MediaBrowser.Controller.Configuration;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Controller.Persistence;
+using MediaBrowser.Controller.Entities;
+using System.Threading;
+using System.Collections;
+
+namespace ItemUpdater
+{
+
+    public class UpdateDetails
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class UpdateItemsDetails
+    {
+        public List<UpdateDetails> UpdateActions { get; set; } = new List<UpdateDetails>();
+    }
+
+    // http://localhost:8096/emby/item_updater/update
+    [Route("/item_updater/update", "POST", Summary = "Update Item")]
+    //[Authenticated]
+    public class UpdateItems : UpdateItemsDetails, IReturn<Object>
+    {
+    }
+
+    // http://localhost:8096/emby/item_updater/ping
+    [Route("/item_updater/ping", "GET", Summary = "PingTest")]
+    //[Authenticated]
+    public class PingTest : IReturn<Object>
+    {
+    }
+
+    public class ApiEndpoint : IService
+    {
+        private readonly ILogger _logger;
+        private readonly ILibraryManager _libraryManager;
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly IItemRepository _itemRepository;
+
+        public ApiEndpoint(ILogManager logger,
+            ILibraryManager libraryManager,
+            IJsonSerializer jsonSerializer,
+            IItemRepository itemRepository)
+        {
+            _logger = logger.GetLogger("ItemUpdater - ApiEndpoint");
+            _libraryManager = libraryManager;
+            _jsonSerializer = jsonSerializer;
+            _itemRepository = itemRepository;
+        }
+
+        public object Get(PingTest request)
+        {
+            Dictionary<string, object> responce = new Dictionary<string, object>();
+            responce["message"] = "ping result";
+            return responce;
+        }
+
+        public object Post(UpdateItems request)
+        {
+            string some_data = _jsonSerializer.SerializeToString(request);
+            _logger.Info("Submitted config data : " + some_data);
+
+            int total_updated = 0;
+            List<BaseItem> updated_items = new List<BaseItem>();
+            foreach(UpdateDetails cr in request.UpdateActions)
+            {
+                bool item_updated = false;
+                long emby_id = long.Parse(cr.Id);
+                BaseItem item = _libraryManager.GetItemById(emby_id);
+
+                if(cr.Type.Equals("CommunityRating", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    float rating = float.Parse(cr.Value);
+                    item.CommunityRating = rating;
+                    item_updated = true;
+                }
+
+                if(item_updated)
+                {
+                    updated_items.Add(item);
+                    total_updated++;
+                }
+            }
+            
+            if(updated_items.Count > 0)
+            {
+                CancellationToken token = CancellationToken.None;
+                _itemRepository.SaveItems(updated_items, token);
+            }
+
+            Dictionary<string, object> responce = new Dictionary<string, object>();
+            responce["message"] = "items updated: " + total_updated;
+            return responce;
+        }
+
+
+    }
+}
---
+++ ItemUpdater/ItemUpdater/Plugin.cs
@@ -0,0 +1,71 @@
+﻿/*
+Copyright(C) 2024
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see<http://www.gnu.org/licenses/>.
+*/
+
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Drawing;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using System;
+using System.IO;
+
+namespace ItemUpdater
+{
+    public class Plugin : BasePlugin<BasePluginConfiguration>, IHasThumbImage
+    {
+        public static Plugin Instance { get; private set; }
+        public static string PluginName = "Emby Item Updater";
+        private Guid _id = new Guid("6081eb61-d53e-4c25-8bd7-8e2575cc1bc2");
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Allows updating individual fields of emby library items";
+            }
+        }
+
+        public override string Name
+        {
+            get { return PluginName; }
+        }
+
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        public ImageFormat ThumbImageFormat
+        {
+            get
+            {
+                return ImageFormat.Png;
+            }
+        }
+
+        public Stream GetThumbImage()
+        {
+            var type = GetType();
+            return type.Assembly.GetManifestResourceStream(type.Namespace + ".Images.thumb.png");
+        }
+
+    }
+}
