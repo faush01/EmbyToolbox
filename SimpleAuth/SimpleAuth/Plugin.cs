d0809cbbe6e738c5ce038b7b7ac07ce89f693549
shaun <shaun@bluebit.com.au>
2024-03-10 20:18:56 +1100

    add simple auth plugin example

---
+++ SimpleAuth/SimpleAuth/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Drawing;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using System;
+using System.IO;
+
+
+namespace SimpleAuth
+{
+    public class Plugin : BasePlugin<BasePluginConfiguration>, IHasThumbImage
+    {
+
+        public static Plugin Instance { get; private set; }
+        public static string PluginName = "Simple Emby Auth";
+        public static string PluginDescription = "Offers simple suth options";
+        private Guid _id = new Guid("0f60d849-3b29-4736-bac0-25b7425ae5b1");
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public Stream GetThumbImage()
+        {
+            var type = GetType();
+            return type.Assembly.GetManifestResourceStream(type.Namespace + ".Images.thumb.png");
+        }
+
+        public ImageFormat ThumbImageFormat
+        {
+            get { return ImageFormat.Png; }
+        }
+
+        public override string Description
+        {
+            get { return PluginDescription; }
+        }
+
+        public override string Name
+        {
+            get { return PluginName; }
+        }
+
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+    }
+}
---
+++ SimpleAuth/SimpleAuth/SimpleAuthProvider.cs
@@ -0,0 +1,81 @@
+﻿using MediaBrowser.Controller.Authentication;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Logging;
+using System;
+using System.Threading.Tasks;
+
+
+namespace SimpleAuth
+{
+    public class SimpleAuthProvider : IAuthenticationProvider
+    {
+        private readonly ILogger _logger;
+        private readonly IUserManager _userManager;
+
+        public SimpleAuthProvider(ILogger logger, IUserManager um) 
+        {
+            _logger = logger;
+            _userManager = um;
+        }
+
+        public string Name
+        {
+            get { return "Simple Auth"; } 
+        }
+
+        public bool IsEnabled
+        { 
+            get { return true; }
+        }
+
+        public Task<ProviderAuthenticationResult> Authenticate(string username, string password)
+        {
+            _logger.Info("Authenticating User : " + username);
+            User user = _userManager.GetUserByName(username);
+            if (user != null)
+            {
+                _logger.Info("User Password    : (" + (user.Password ?? "null") + ")");
+                _logger.Info("Entered Password : (" + (password ?? "null") + ")");
+                if (string.IsNullOrEmpty(user.Password) && string.IsNullOrEmpty(password))
+                {
+                    _logger.Info("Login worked (empty)");
+                    return Task.FromResult(new ProviderAuthenticationResult());
+                }
+                else if (user.Password == password)
+                {
+                    _logger.Info("Login worked (matched)");
+                    return Task.FromResult(new ProviderAuthenticationResult());
+                }
+                else
+                {
+                    _logger.Info("Login failed - Incorrect Password");
+                    throw new Exception("Login failed - Incorrect Password");
+                }
+            }
+            else
+            {
+                _logger.Info("Login failed - User Not Found");
+                throw new Exception("Login failed - User Not Found");
+            }
+        }
+
+        public Task<bool> HasPassword(User user)
+        {
+            bool has_pw = true;
+            if(string.IsNullOrEmpty(user.Password))
+            {
+                has_pw = false;
+            }
+            return Task.FromResult(has_pw);
+        }
+
+        public Task ChangePassword(User user, string newPassword)
+        {
+            user.Password = newPassword;
+            _userManager.UpdateUser(user);
+            return Task.CompletedTask;
+        }
+
+    }
+}
