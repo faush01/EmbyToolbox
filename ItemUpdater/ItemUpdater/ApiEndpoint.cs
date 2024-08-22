5d25bef6193fe1b4063a9ec4a95b1f66fc0eeb63
shaun <shaun@bluebit.com.au>
2024-08-22 11:24:11 +1000

    accept multiple action for each emby item

--- ItemUpdater/ItemUpdater/ApiEndpoint.cs
+++ ItemUpdater/ItemUpdater/ApiEndpoint.cs
@@ -13,26 +13,22 @@ using MediaBrowser.Controller.Persistence;
 using MediaBrowser.Controller.Entities;
 using System.Threading;
 using System.Collections;
+using MediaBrowser.Model.Entities;
+using System.Linq;
 
 namespace ItemUpdater
 {
 
     public class UpdateDetails
     {
-        public string Id { get; set; }
         public string Type { get; set; }
         public string Value { get; set; }
-    }
-
-    public class UpdateItemsDetails
-    {
-        public List<UpdateDetails> UpdateActions { get; set; } = new List<UpdateDetails>();
     }
 
     // http://localhost:8096/emby/item_updater/update
     [Route("/item_updater/update", "POST", Summary = "Update Item")]
     //[Authenticated]
-    public class UpdateItems : UpdateItemsDetails, IReturn<Object>
+    public class UpdateItems : Dictionary<string, List<UpdateDetails>>, IReturn<Object>
     {
     }
 
@@ -74,21 +70,52 @@ namespace ItemUpdater
             _logger.Info("Submitted config data : " + some_data);
 
             int total_updated = 0;
+            int total_action = 0;
             List<BaseItem> updated_items = new List<BaseItem>();
-            foreach(UpdateDetails cr in request.UpdateActions)
+            foreach(var item_actions in request)
             {
                 bool item_updated = false;
-                long emby_id = long.Parse(cr.Id);
+                long emby_id = long.Parse(item_actions.Key);
                 BaseItem item = _libraryManager.GetItemById(emby_id);
 
-                if(cr.Type.Equals("CommunityRating", StringComparison.CurrentCultureIgnoreCase))
+                if (item == null)
                 {
-                    float rating = float.Parse(cr.Value);
-                    item.CommunityRating = rating;
-                    item_updated = true;
+                    _logger.Info("Item not found : " + emby_id);
+                    continue;
                 }
 
-                if(item_updated)
+                _logger.Info("Processing changes for : " + emby_id);
+                foreach (var action in item_actions.Value)
+                {
+                    if (action.Type.Equals("CommunityRating", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        double rating = double.Parse(action.Value);
+                        rating = Math.Round(rating, 1);
+                        int diff = (int)(rating * 10) - (int)((item.CommunityRating ?? 0) * 10);
+                        if (diff != 0)
+                        {
+                            _logger.Info("Updating Rating " + rating);
+                            item.CommunityRating = (float)rating;
+                            item_updated = true;
+                            total_action++;
+                        }
+                    }
+
+                    if (action.Type.Equals("LockField", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        MetadataFields target_filed = (MetadataFields)Enum.Parse(typeof(MetadataFields), action.Value, true);
+                        List<MetadataFields> metadataFields = new List<MetadataFields>(item.LockedFields);
+                        if (!metadataFields.Contains(target_filed))
+                        {
+                            metadataFields.Add(target_filed);
+                            item.LockedFields = metadataFields.ToArray();
+                            item_updated = true;
+                            total_action++;
+                        }
+                    }
+                }
+
+                if (item_updated)
                 {
                     updated_items.Add(item);
                     total_updated++;
@@ -102,7 +129,8 @@ namespace ItemUpdater
             }
 
             Dictionary<string, object> responce = new Dictionary<string, object>();
-            responce["message"] = "items updated: " + total_updated;
+            responce["total_items"] = total_updated;
+            responce["total_actions"] = total_action;
             return responce;
         }
 
